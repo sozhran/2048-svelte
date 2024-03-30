0de6ce081e8bd9dc2ac1320abee1b701d13fe360
John <sozhran@gmail.com>
2024-03-30 23:04:22 +0100

    fixed footer position

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,29 +7,4 @@
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<a href="https://github.com/sozhran/2048" target="_blank">
-			<img alt="github share icon" src="https://i.imgur.com/5Qr1cEC.png" width="30" height="30" />
-		</a>
-	</footer>
 </div>
-
-<style>
-	app {
-		display: flex;
-		flex-direction: column;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			display: flex;
-			align-self: flex-end;
-			flex-direction: row;
-			justify-content: center;
-			align-items: bottom;
-			margin: 10px;
-			opacity: 60%;
-		}
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -115,47 +115,61 @@
 	<meta name="description" content="2048" />
 </svelte:head>
 
-<section class="top-section">
-	<h1>2048</h1>
-	<!-- <button on:click={toggleGameOver}>G.O.</button> -->
-	{#if gameOver === true}
-		<h1 class="red">GAME OVER</h1>
-	{:else}
-		<h1 class="red" style="visibility:hidden">GAME OVER</h1>
-	{/if}
-	<!-- <hr /> -->
-	<p>Score: {score}</p>
-</section>
-
-<div class="main-section">
-	<div class="board-section">
-		<img alt="unicorn" src={unicorn} width="300px" height="300px" />
-		<div class="board">
-			{#each board as row, rowIndex}
-				{#each row as tile, tileIndex}
-					<div
-						class={tile > 4096
-							? tileChecker(newTiles, rowIndex, tileIndex)
-								? 'tile x4096' + ' newtile'
-								: 'tile x4096'
-							: tileChecker(newTiles, rowIndex, tileIndex)
-								? 'tile x' + tile + ' newtile'
-								: 'tile x' + tile}
-					>
-						{#if tile > 0}{tile}{/if}
-					</div>
-				{/each}
-			{/each}
-		</div>
-		<img alt="wolf" src={wolf} width="300px" height="300px" />
-	</div>
-	<div class="game-over">
-		{#if gameOver}
-			<button on:click={resetGame}>Play again</button>
+<body>
+	<section class="top-section">
+		<h1>2048</h1>
+		<!-- <button on:click={toggleGameOver}>G.O.</button> -->
+		{#if gameOver === true}
+			<h1 class="red">GAME OVER</h1>
 		{:else}
-			<button on:click={resetGame} disabled>Play again</button>
+			<h1 class="red" style="visibility:hidden">GAME OVER</h1>
 		{/if}
-	</div>
-</div>
+		<!-- <hr /> -->
+		<p>Score: {score}</p>
+	</section>
+
+	<section class="main-section">
+		<div class="board-section">
+			<img alt="unicorn" src={unicorn} width="300px" height="300px" />
+			<div class="board">
+				{#each board as row, rowIndex}
+					{#each row as tile, tileIndex}
+						<div
+							class={tile > 4096
+								? tileChecker(newTiles, rowIndex, tileIndex)
+									? 'tile x4096' + ' newtile'
+									: 'tile x4096'
+								: tileChecker(newTiles, rowIndex, tileIndex)
+									? 'tile x' + tile + ' newtile'
+									: 'tile x' + tile}
+						>
+							{#if tile > 0}{tile}{/if}
+						</div>
+					{/each}
+				{/each}
+			</div>
+			<img alt="wolf" src={wolf} width="300px" height="300px" />
+		</div>
+		<div class="game-over">
+			{#if gameOver}
+				<button on:click={resetGame}>Play again</button>
+			{:else}
+				<button on:click={resetGame} disabled>Play again</button>
+			{/if}
+		</div>
+	</section>
+
+	<section class="footer">
+		<a href="https://github.com/sozhran/2048" target="_blank">
+			<img
+				alt="github share icon"
+				src="https://i.imgur.com/5Qr1cEC.png"
+				width="25"
+				height="25"
+				class="share"
+			/>
+		</a>
+	</section>
+</body>
 
 <svelte:window on:keyup|preventDefault={handleKeyPress} />
